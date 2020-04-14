ab743cc5d5f296ab0259559d6e293290b1ec0599
Jan Dvořák <xdvora2m@vutbr.cz>
2020-04-14 09:22:46 +0200

    Additional tests added

---
+++ ParserTests/RequestTests.cs
@@ -0,0 +1,93 @@
+using HTTP_Parser;
+using System;
+using System.IO;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ParserTests
+{
+    public class RequestTests
+    {
+        private readonly ITestOutputHelper output;
+        public RequestTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void HttpRequest_Test()
+        {
+            string input;
+            using (FileStream fs = new FileStream("Resources/Requests/http_get.bin", FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+                input = Encoding.ASCII.GetString(bin, 0, bin.Length);
+            }
+            var res = HttpParser.Parse(input);
+            Assert.True(res.Success);
+            output.WriteLine(res.Value.ToString());
+        }
+
+        [Fact]
+        public void HttpRequestToOriginServer()
+        {
+            string input;
+            using (FileStream fs = new FileStream("Resources/Requests/http_request_to_origin_server.bin", FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+                input = Encoding.ASCII.GetString(bin, 0, bin.Length);
+            }
+            var res = HttpParser.Parse(input);
+            Assert.True(res.Success);
+            output.WriteLine(res.Value.ToString());
+        }
+
+        [Fact]
+        public void HttpRequestToIPv4Address()
+        {
+            string input;
+            using (FileStream fs = new FileStream("Resources/Requests/http_request_to_IPv4_address.bin", FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+                input = Encoding.ASCII.GetString(bin, 0, bin.Length);
+            }
+            var res = HttpParser.Parse(input);
+            Assert.True(res.Success);
+            output.WriteLine(res.Value.ToString());
+        }
+
+        [Fact]
+        public void HttpRequestPost()
+        {
+            string input;
+            using (FileStream fs = new FileStream("Resources/Requests/http_request_post.bin", FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+                input = Encoding.ASCII.GetString(bin, 0, bin.Length);
+            }
+            var res = HttpParser.Parse(input);
+            Assert.True(res.Success);
+            output.WriteLine(res.Value.ToString());
+        }
+
+        [Fact]
+        public void HttpRequestWithMultipleQueries()
+        {
+            string input;
+            using (FileStream fs = new FileStream("Resources/Requests/http_request_with_multiple_queries.bin", FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+                input = Encoding.ASCII.GetString(bin, 0, bin.Length);
+            }
+            var res = HttpParser.Parse(input);
+            Assert.True(res.Success);
+            output.WriteLine(res.Value.ToString());
+        }
+    }
+}
---
+++ ParserTests/ResponseTests.cs
@@ -0,0 +1,82 @@
+﻿using HTTP_Parser;
+using System;
+using System.IO;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ParserTests
+{
+    public class ResponseTests
+    {
+        private readonly ITestOutputHelper output;
+        public ResponseTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void HttpResponseWithoutBody()
+        {
+            string input;
+            using (FileStream fs = new FileStream("Resources/Responses/http_response_without_body.bin", FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+                input = Encoding.ASCII.GetString(bin, 0, bin.Length);
+            }
+            var res = HttpParser.Parse(input);
+            Assert.True(res.Success);
+            output.WriteLine(res.Value.ToString());
+        }
+
+        [Fact]
+        public void HttpResponseWithTextTest()
+        {
+            string input;
+            using (FileStream fs = new FileStream("Resources/Responses/http_response_with_text.bin", FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+                input = Encoding.ASCII.GetString(bin, 0, bin.Length);
+            }
+            var res = HttpParser.Parse(input);
+            Assert.True(res.Success);
+            output.WriteLine(res.Value.ToString());
+            output.WriteLine(res.Value.MessageBody.Length.ToString());
+        }
+
+        [Fact]
+        public void HttpResponseWithJpeg()
+        {
+            string input;
+            using (FileStream fs = new FileStream("Resources/Responses/http_response_with_jpeg.bin", FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+                input = Encoding.ASCII.GetString(bin, 0, bin.Length);
+            }
+
+            var res = HttpParser.Parse(input);
+            Assert.True(res.Success);
+            output.WriteLine(res.Value.ToString());
+            output.WriteLine(res.Value.MessageBody.Length.ToString());
+        }
+
+        [Fact]
+        public void HttpResponseWithWebPage()
+        {
+            string input;
+            using (FileStream fs = new FileStream("Resources/Responses/http_response_webPage.bin", FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                byte[] bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+                input = Encoding.ASCII.GetString(bin, 0, bin.Length);
+            }
+            var res = HttpParser.Parse(input);
+            Assert.True(res.Success);
+            output.WriteLine(res.Value.ToString());
+            output.WriteLine(res.Value.MessageBody.Length.ToString());
+        }
+    }
+}
