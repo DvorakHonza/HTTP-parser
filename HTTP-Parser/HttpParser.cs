e7b3c7f826a5c9f3699d5e2a9809ca2420b03d20
Jan Dvořák <xdvora2m@vutbr.cz>
2020-04-16 12:44:20 +0200

    Fixed percent encoding + refactoring of LINQ

--- HTTP-Parser/HttpParser.cs
+++ HTTP-Parser/HttpParser.cs
@@ -19,13 +19,13 @@ namespace HTTP_Parser
         private static readonly char[] VCharComplement = { '\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07', '\x08', '\x09', '\x0a', '\x0b', '\x0c',
                                                            '\x0d', '\x0e', '\x0f', '\x10', '\x11' ,'\x12', '\x13', '\x14', '\x15' ,'\x16', '\x17', '\x18', '\x19',
                                                            '\x1a', '\x1b', '\x1c', '\x1d', '\x1e', '\x1f', '\x20', '\x7f'};
+        private static readonly char[] HexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F' };
 
         private static readonly Parser<char, char> Slash = Char('/');
         private static readonly Parser<char, char> Dot = Char('.');
         private static readonly Parser<char, char> Space = Char(' ');
         private static readonly Parser<char, char> HTab = Char('\t');
         private static readonly Parser<char, char> QuestionMark = Char('?');
-        private static readonly Parser<char, char> EqualsSign = Char('=');
         private static readonly Parser<char, string> Http = String("HTTP");
         private static readonly Parser<char, string> Asterisk = String("*");
         private static readonly Parser<char, string> CRLF = String("\r\n");
@@ -45,14 +45,17 @@ namespace HTTP_Parser
         private static readonly Parser<char, char> RightBracket = Char(']');
         private static readonly Parser<char, char> Unreserved = OneOf(new List<Parser<char, char>>() { LetterOrDigit, Dash, Dot, Underscore, Tilde }).Labelled("Unreserved");
         private static readonly Parser<char, char> Percent = Char('%');
+        private static readonly Parser<char, char> HexDigit = OneOf(HexDigits);
         private static readonly Parser<char, char> PercentEncoding =
             from percent in Percent
-            from number in HexNum.Where(res => res >= 0 && res <= 255).Labelled("PercentEncoding hex value")
-            select Convert.ToChar(Convert.ToUInt32(number));
+            from number in HexDigit.Repeat(2).Select(res => ConvertIEnumerableToString(res))
+            select Convert.ToChar(Convert.ToUInt32(number, 16));
 
         private static readonly Parser<char, string> Scheme = String("http");
+
         private static readonly Parser<char, string> UserInfo =
             OneOf(new List<Parser<char, char>>() { Unreserved, PercentEncoding, UriSubDelims, Colon }).ManyString().Labelled("User info");
+
         private static readonly Parser<char, string> IpVFuture =
             from beginning in Char('v').Labelled("IpVFuture v")
             from version in HexNum.Labelled("IpVFuture version")
@@ -61,28 +64,30 @@ namespace HTTP_Parser
             select beginning.ToString() + version.ToString() + dot.ToString() + ConvertIEnumerableToString(end);
 
         private static readonly Parser<char, int> DecOctet = DecimalNum.Where(res => res >= 0 && res <= 255).Labelled("Decimal Octet");
-        private static readonly Parser<char, List<int>> IPv4AddressOctets =
-            from firstOctet in DecOctet.Before(Dot)
-            from secondOctet in DecOctet.Before(Dot)
-            from thirdOctet in DecOctet.Before(Dot)
-            from fourthOctet in DecOctet.Before(Whitespace) //TODO What if ip address is not followed by whitespace
-            select new List<int>() { firstOctet, secondOctet, thirdOctet, fourthOctet };
-        private static readonly Parser<char, string> IPv4Address = IPv4AddressOctets.Select(res => IPv4ToDotted(res)).Labelled("IPv4Address");
 
-        private static readonly Parser<char, int> h16 = HexNum.Where(res => res >= 0 && res <= 0xFFFF).Labelled("16 bits of IPv6 Address");
+        private static readonly Parser<char, List<int>> IPv4AddressOctets =
+            Map((first, second, third, fourth) => new List<int>() { first, second, third, fourth },
+                DecOctet.Before(Dot),
+                DecOctet.Before(Dot),
+                DecOctet.Before(Dot),
+                DecOctet.Before(SkipWhitespaces)
+                ).Labelled("IPv4Address octets");
+
+        private static readonly Parser<char, string> IPv4Address = 
+            IPv4AddressOctets.Select(res => IPv4ToDotted(res)).Labelled("IPv4Address");
+
+        private static readonly Parser<char, int> h16 = 
+            HexNum.Where(res => res >= 0 && res <= 0xFFFF).Labelled("16 bits of IPv6 Address");
+
         private static readonly Parser<char, string> ls32 =
-            h16.Separated(Colon).Select(res => res.ToString()).Or(IPv4AddressOctets.Select(res => IPv4ToHex(res))).Labelled("32 least significant bits of IPv6 address");
+            h16.Separated(Colon).Select(res => res.ToString()).
+            Or(IPv4AddressOctets.Select(res => IPv4ToHex(res))).Labelled("32 least significant bits of IPv6 address");
 
         private static readonly Parser<char, string> IPv6WithoutShortening =
-            from begin in h16.Select(res => res.ToString("X") + ":").Repeat(6)
-            from rest in ls32
-            select begin + rest;
+            Map((begin, rest) => begin + rest, h16.Select(res => res.ToString("X") + ":").Repeat(6), ls32);
 
         private static readonly Parser<char, string> IPv6First16BitsShortened =
-            from doubleColon in DoubleColon
-            from begin in h16.Select(res => res.ToString("X") + ":").Repeat(5)
-            from rest in ls32
-            select doubleColon + begin + rest;
+            Map((begin, rest) => "::" + begin + rest, DoubleColon.Then(h16.Select(res => res.ToString("X") + ":").Repeat(5)), ls32);
 
         private static readonly Parser<char, string> IPv6Address =
             OneOf(new List<Parser<char, string>>() {
@@ -90,28 +95,33 @@ namespace HTTP_Parser
                 IPv6First16BitsShortened
             }).Labelled("IPv6Address"); //TODO IPv6 address parsing
 
-        private static readonly Parser<char, string> RegistredName = Unreserved.Or(PercentEncoding).Or(UriSubDelims).ManyString().Labelled("RegistredName");
+        private static readonly Parser<char, string> RegistredName =
+            Unreserved.Or(PercentEncoding).Or(UriSubDelims).ManyString().Labelled("RegistredName");
 
-        private static readonly Parser<char, string> IpLiteral = IpVFuture.Or(IPv6Address).Between(LeftBracket, RightBracket).Labelled("IpLiteral");
-        private static readonly Parser<char, string> Host = IpLiteral.Or(IPv4Address).Or(RegistredName).Labelled("Host");
+        private static readonly Parser<char, string> IpLiteral = 
+            IpVFuture.Or(IPv6Address).Between(LeftBracket, RightBracket).Labelled("IpLiteral");
+
+        private static readonly Parser<char, string> Host = 
+            IpLiteral.Or(IPv4Address).Or(RegistredName).Labelled("Host");
 
         private static readonly Parser<char, RequestTarget> Authority =
-            from userInfo in UserInfo.Labelled("Authority UserInfo")
-            from atSign in AtSign.Labelled("Authority at sign")
-            from host in Host.Labelled("Authority host")
-            from colon in Colon.Labelled("Authority Colon")
-            from port in DecimalNum.Labelled("Authority port")
-            select new AuthorityForm(userInfo + atSign.ToString() + host, port) as RequestTarget;
+            Map((userInfo, host, port) => new AuthorityForm(userInfo + "@" + host, port) as RequestTarget,
+                UserInfo.Before(AtSign),
+                Host.Before(Colon),
+                DecimalNum
+                );
 
         private static readonly Parser<char, char> PChar =
             OneOf(new List<Parser<char, char>>() { Unreserved, PercentEncoding, UriSubDelims, Colon, AtSign }).Labelled("pchar");
-        private static readonly Parser<char, string> PathAbempty = Slash.Then(PChar.ManyString()).ManyString().Select(res => "/" + res.ToString()).Labelled("Path AbEmpty");
-        private static readonly Parser<char, string> SegmentNz = PChar.AtLeastOnce().Select(res => ConvertIEnumerableToString(res)).Labelled("SegmentNz");
+
+        private static readonly Parser<char, string> PathAbempty = 
+            Slash.Then(PChar.ManyString()).ManyString().Select(res => "/" + res.ToString()).Labelled("Path AbEmpty");
+
+        private static readonly Parser<char, string> SegmentNz = 
+            PChar.AtLeastOnce().Select(res => ConvertIEnumerableToString(res)).Labelled("SegmentNz");
 
         private static readonly Parser<char, string> PathAbsoluteOptional =
-            from segment in SegmentNz
-            from pathAbempty in PathAbempty
-            select segment + pathAbempty;
+            Map((segment, pathAbempty) => segment + pathAbempty, SegmentNz, PathAbempty);
 
         private static readonly Parser<char, string> PathAbsolute =
             from slash in Slash
@@ -119,60 +129,54 @@ namespace HTTP_Parser
             select optional.HasValue ? optional.Value : "/";
 
         private static readonly Parser<char, string> PathRootless =
-            from segmentNz in SegmentNz
-            from pathAbempty in PathAbempty
-            select segmentNz + PathAbempty;
+            Map((segmentNz, pathAbempty) => segmentNz + pathAbempty, SegmentNz, PathAbempty);
 
         private static readonly Parser<char, string> PathEmpty = PChar.RepeatString(0).Labelled("Path empty");
 
         private static readonly Parser<char, string> Path = PathAbempty.Or(PathAbsolute).Or(PathRootless).Or(PathEmpty).Labelled("Path");
 
         private static readonly Parser<char, string> HierPart =
-            from authority in DoubleSlash.Then(Authority).Labelled("HierPart Authority")
-            from path in Path.Labelled("HierPart Path")
-            select authority + path;
+            Map((authority, path) => authority + path, DoubleSlash.Then(Authority), Path).Labelled("HierPart");
 
-        private static readonly Parser<char, string> Fragment = NumberSign.Then(PChar.Or(Slash).Or(QuestionMark).ManyString()).Select(res => "#" + res).Labelled("Fragment");
+        private static readonly Parser<char, string> Fragment = 
+            NumberSign.Then(PChar.Or(Slash).Or(QuestionMark).ManyString()).Select(res => "#" + res).Labelled("Fragment");
 
         private static readonly Parser<char, double> Version = Http.Then(Slash).Then(Real).Labelled("Version");
         //TODO add obs-text
         private static readonly Parser<char, string> ReasonPhrase = LetterOrDigit.Or(Space).Or(HTab).ManyString().Labelled("ReasonPhrase");
         private static readonly Parser<char, StartLine> StatusLineParser =
-           from version in Version.Before(Space).Labelled("Status line version")
-           from statusCode in DecimalNum.Before(Space).Labelled("Status line code")
-           from reasonPhrase in ReasonPhrase.Before(CRLF).Labelled("Status line reason phrase")
-           select new StatusLine(version, statusCode, reasonPhrase) as StartLine;
+            Map((version, statusCode, reasonPhrase) => new StatusLine(version, statusCode, reasonPhrase) as StartLine,
+                Version.Before(Space),
+                DecimalNum.Before(Space),
+                ReasonPhrase.Before(CRLF)
+                ).Labelled("StatusLineParser");
 
         private static readonly Parser<char, string> Method = Letter.AtLeastOnce().Select(res => ConvertIEnumerableToString(res)).Labelled("Method");
 
         private static readonly Parser<char, string> Segment = PChar.ManyString();
 
-        private static readonly Parser<char, string> AbsolutePath =
-            from absolutePath in (Slash.Then(Segment)).AtLeastOnce().Select(res => PrependChar(res, '/'))
-            select absolutePath;
+        private static readonly Parser<char, string> AbsolutePath = Slash.Then(Segment).AtLeastOnce().Select(res => PrependChar(res, '/'));
 
-        private static readonly Parser<char, KeyValuePair<string, string>> Query =
-            from name in LetterOrDigit.AtLeastOnce().Before(EqualsSign).Select(res => ConvertIEnumerableToString(res)).Labelled("Query name")
-            from value in LetterOrDigit.AtLeastOnce().Select(res => ConvertIEnumerableToString(res)).Labelled("Query value")
-            select new KeyValuePair<string, string>(name, value);
+        private static readonly Parser<char, KeyValuePair<string, string>> Query = PChar.Or(Slash).Or(QuestionMark).ManyString().Select(res => QueryToKVPair(res));
 
         private static readonly Parser<char, ImmutableDictionary<string, string>> Queries = 
             Query.Separated(Char('&')).Select(kvps => kvps.ToImmutableDictionary()).Labelled("Queries");
 
         private static readonly Parser<char, RequestTarget> OriginForm =
             from absolutePath in AbsolutePath.Labelled("OriginForm absolute path")
-            from queries in Queries.Optional().Labelled("Optional query")
+            from queries in QuestionMark.Then(Queries).Optional().Labelled("Optional query")
             from space in Space
             select queries.HasValue ? 
                 new OriginForm(absolutePath, queries.Value) as RequestTarget : 
-                new OriginForm(absolutePath) as RequestTarget; 
+                new OriginForm(absolutePath) as RequestTarget;
 
         private static readonly Parser<char, RequestTarget> AbsoluteFormParser =
-           from scheme in Scheme.Before(Colon).Labelled("AbsoluteUri scheme")
-           from hierPart in HierPart.Labelled("AbsoluteUri hierPart")
-           from queries in QuestionMark.Then(Queries).Optional().Labelled("AbsoluteUri query")
-           from fragment in Fragment.Optional().Labelled("AbsoluteUri fragment")
-           select FormatAbsoluteUriOutput(scheme, hierPart, queries, fragment) as RequestTarget;
+           Map((scheme, hierPart, queries, fragment) => FormatAbsoluteUriOutput(scheme, hierPart, queries, fragment) as RequestTarget,
+               Scheme.Before(Colon),
+               HierPart,
+               QuestionMark.Then(Queries).Optional(),
+               Fragment.Optional()
+               );
 
         private static readonly Parser<char, RequestTarget> AsteriskFormParser = 
             Asterisk.Between(Space).Select(res => new AsteriskForm() as RequestTarget).Labelled("Asterisk form");
@@ -181,10 +185,11 @@ namespace HTTP_Parser
             OriginForm.Or(AsteriskFormParser).Or(AbsoluteFormParser).Or(Authority).Labelled("Request target");
 
         private static readonly Parser<char, StartLine> RequestLineParser =
-            from method in Method.Before(Space).Labelled("RequestLine method")
-            from requestTarget in RequestTargetParser//.Between(SkipWhitespaces).Labelled("RequestLine request target")
-            from version in Version.Before(CRLF).Labelled("RequestLine version")
-            select new RequestLine(method, requestTarget, version) as StartLine;
+            Map((method, requestTarget, version) => new RequestLine(method, requestTarget, version) as StartLine,
+                Method.Before(Space),
+                RequestTargetParser,
+                Version.Before(CRLF)
+                );
 
         private static readonly Parser<char, StartLine> StartLineParser = StatusLineParser.Or(RequestLineParser).Labelled("StartLine");
 
@@ -195,45 +200,41 @@ namespace HTTP_Parser
         private static readonly Parser<char, char> FieldVChar = AnyCharExcept(VCharComplement).Labelled("FieldVChar");
 
         private static readonly Parser<char, string> FieldContentOptional =
-            from space in Space.Or(HTab).Labelled("FieldContentOptional space")
-            from trailingSpaces in Space.Or(HTab).ManyString().Labelled("FieldContentOptional trailingSpaces")
-            from vchar in FieldVChar.Labelled("FieldContentOptional VChar")
-            select space + trailingSpaces + vchar;
-
+            Map((spaces, vchar) => spaces + vchar,
+                Space.Or(HTab).AtLeastOnce().Select(res => ConvertIEnumerableToString(res)),
+                FieldVChar
+                );
+            
         private static readonly Parser<char, string> FieldContent =
             from begin in FieldVChar.Labelled("FieldContent begin")
             from rest in FieldContentOptional.Optional().Labelled("FieldContent rest")
             select rest.HasValue ? begin + rest.Value : begin.ToString();
 
-        private static readonly Parser<char, string> ObsFold =
-            from crlf in CRLF.Labelled("ObsFold crlf")
-            from space in Space.Or(HTab).Labelled("ObsFold space")
-            from trailingSpace in Space.Or(HTab).Labelled("ObsFold trailingSpace")
-            select crlf + space + trailingSpace;
+        private static readonly Parser<char, Unit> ObsFold =
+            CRLF.Then(Space.Or(HTab).AtLeastOnce()).SkipMany();
+
         private static readonly Parser<char, string> FieldValue = FieldContent.ManyString();//.Or(ObsFold).ManyString();
         private static readonly Parser<char, string> WhitespacesExceptCrLf = Char('\x09').Or(Char('\x0b')).Or(Char('\x0c')).ManyString();
 
         private static readonly Parser<char, KeyValuePair<string, string>> HeaderFieldParser =
             FieldName.Before(ColonWhitespace)
             .Then(FieldValue, (key, value) => new KeyValuePair<string, string>(key, value)).Before(CRLF);
+
         private static readonly Parser<char, ImmutableDictionary<string, string>> HeaderFields =
             HeaderFieldParser.Many().Select(kvps => kvps.ToImmutableDictionary());
 
         private static readonly Parser<char, HttpHeader> HttpHeaderParser =
-            from startLine in StartLineParser
-            from headerFields in HeaderFields
-            select new HttpHeader(startLine, headerFields);
+            Map((startLine, headerFields) => new HttpHeader(startLine, headerFields), StartLineParser, HeaderFields);
+
 
         private static readonly Parser<char, HttpMessage> HttpMessageParser =
             from header in HttpHeaderParser.Before(CRLF)
             from body in Any.Select(res => (byte)res).Repeat(header.GetMessageBodyLength()).Select(res => res.ToArray()).Optional()
             select body.HasValue ? new HttpMessage(header, body.Value) : new HttpMessage(header);
 
-        private static readonly Parser<char, double> test =
-            from t in Http.Then(Slash).Then(Real)
-            select t;
+        private static readonly Parser<char, char> test = PercentEncoding;
 
-        //public static Result<char, string> Parse(string input) => test.Parse(input);
+        //public static Result<char, char> Parse(string input) => test.Parse(input);
         public static Result<char, HttpMessage> Parse(string input) => HttpMessageParser.Parse(input);
 
         private static string IPv4ToHex(List<int> octets)
@@ -284,6 +285,12 @@ namespace HTTP_Parser
                 stringBuilder.Append(segment);
             }
             return stringBuilder.ToString();
+        }
+
+        private static KeyValuePair<string, string> QueryToKVPair(string query)
+        {
+            var parts = query.Split(new[] { '=' }, 2);
+            return new KeyValuePair<string, string>(parts[0], parts[1]);
         }
     }
 }
